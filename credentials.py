8050d6a4702bab81059254392ad65cee261ace37
Ankush Pandey <79956433+Ankush2201@users.noreply.github.com>
2024-09-25 14:57:01 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -5,4 +5,4 @@ class Credentials:
     MYSQL_USER = 'root' # this will be remain root since we create it while creating Docker image
     MYSQL_PASSWORD = '123456' # created while creating Docker image
     MYSQL_DB_NAME = 'stock_market' #  you can change it
-    NTYIMTES_API_KEY = 'iJH2eJaKHYE9C02yVYwiEAqG649Hb8L7' # Get your wown new york times api key 
+    NTYIMTES_API_KEY = '' # Get your wown new york times api key 
